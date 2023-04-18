b5eebb67ba83ae4729688bd30ca77515a59b20a4
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-04-18 05:52:28 +0200

    Added data to display information about the rover

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,18 @@
-function About() {
+import React from "react";
+
+function About({ data, selectedRover }) {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h2>{selectedRover}</h2>
+      {data && (
+        <div>
+          <p>Landing Date: {data.rover.landing_date}</p>
+          <p>Launch Date: {data.rover.launch_date}</p>
+          <p>Status: {data.rover.status}</p>
+        </div>
+      )}
+    </div>
+  );
 }
-export default About
+
+export default About;
