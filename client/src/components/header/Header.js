cb9c2c90d603086129eb97672f0f0a9b3e7c82ea
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-04-20 01:49:22 +0200

    Initial commit - General layout - vizualizing later development layout

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,21 +3,20 @@ import RoverSelection from '../rover/RoverSelection';
 
 function Header( {setSelectedRover }) {
   return (
-    <div className='bg-red-500'>
+    <div>
 
-      <div className='flex bg-green-500 m-1'>
+      <div className=' bg-green-500 m-1'>
         {/* Create Mars Logo */}
         {/* <div className='flex'>
           <img src='https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg' alt='Mars Logo' className='w-20 h-20' />
         </div> */}
 
         {/* Create welcome martian text in center of div*/}
-        <div className='flex justify-center items-center w-full'>
-          <h1 className='text-4xl text-white'>Welcome Martian</h1>
-          <RoverSelection setSelectedRover={setSelectedRover} />
-            </div>
+        <div className='flex'>
+          <h1 className='text-4xl text-orange-600'>Welcome Martian</h1>
+        </div>
       </div>
-
+      <RoverSelection setSelectedRover={setSelectedRover} />
     </div>
   )
 }
