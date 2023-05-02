52acd5df3784ad55c6038535e329d8365b6387b0
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-02 04:19:32 +0200

    Added custom bg-styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'nasa-ofTheDay': "url('/src/assets/pages/home/Home_BG.jpg')",
+      }
+    }
   },
   plugins: [],
 }
