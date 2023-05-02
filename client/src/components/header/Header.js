7246b11344f723eab70fceca81e64af1286bf39c
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-02 02:57:25 +0200

    Added home page

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,14 +3,22 @@ import RoverSelection from '../rover/RoverSelection';
 
 function Header( {setSelectedRover }) {
   return (
-    <div className='header__container text-center'>
-      <div className='flex bg-green-500 m-1 justify-center'>
-        <div className=' bg-green-500 m-1 '>
+    <div className='header__container text-center flex bg-slate-500'>
+      {/* Home Button */}
+      <div className="home_button_container flex">
+        <button className="home_button bg-slate-600 hover:bg-green-700 text-white font-bold py-2 px-4">
+          Home
+        </button>
+      </div>
+      {/* Salutation div */}
+      <div className='flex m-1 justify-center'>
+        <div className='m-1 '>
           <div class='flex'>
-            <h1 class='text-4xl text-orange-600 '>Welcome Martian</h1>
+            <h1 class='text-3xl'>Welcome Martian</h1>
           </div>
         </div>
       </div>
+      {/* Get Rover Buttons */}
       <div className="rover__selection__container">
         <RoverSelection setSelectedRover={setSelectedRover} />
       </div>
