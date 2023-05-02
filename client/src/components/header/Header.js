e9a11ec2168d68d6897cc0d1c6221f1a2e73467e
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-02 02:16:09 +0200

    Cleaned up header component - removed unnecessary DOM elements

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,21 +3,17 @@ import RoverSelection from '../rover/RoverSelection';
 
 function Header( {setSelectedRover }) {
   return (
-    <div>
-
-      <div className=' bg-green-500 m-1'>
-        {/* Create Mars Logo */}
-        {/* <div className='flex'>
-          <img src='https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg' alt='Mars Logo' className='w-20 h-20' />
-        </div> */}
-
-        {/* Create welcome martian text in center of div*/}
-        <div className='flex'>
-          <h1 className='text-4xl text-orange-600'>Welcome Martian</h1>
+    <div className='header__container text-center'>
+      <div className='flex bg-green-500 m-1 justify-center'>
+        <div className=' bg-green-500 m-1 '>
+          <div class='flex'>
+            <h1 class='text-4xl text-orange-600 '>Welcome Martian</h1>
+          </div>
         </div>
       </div>
-      <RoverSelection setSelectedRover={setSelectedRover} />
-      
+      <div className="rover__selection__container">
+        <RoverSelection setSelectedRover={setSelectedRover} />
+      </div>
     </div>
   )
 }
