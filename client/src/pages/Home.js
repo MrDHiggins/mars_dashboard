e9b0a82c2494d1913380cd65e43b739491e29ae4
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-02 04:20:27 +0200

    Styling of home page / root path

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='flex w-screen h-screen'>
+        <div className="home__container h-full w-full bg-nasa-ofTheDay p-2">
+            <div className="bg-red h-full w-full bg-gray-400 bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg">
+                {/* Black hole week container */}
+                <div className="black__hole__container">
+
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 export default Home
