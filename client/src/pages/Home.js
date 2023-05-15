d7fca0fe8c8ce778596a3244baa7ab4ea80f3f14
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-15 03:02:08 +0200

    Adjusted parent div width and added MarsFacts component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import MarsFacts from '../components/home/MarsFacts';
 
 function Home() {
   return (
-    <div className='flex w-screen h-screen'>
+    <div className='flex min-w-screen h-screen'>
         <div className="home__container h-full w-full bg-nasa-ofTheDay p-2">
             <div className="bg-red h-full w-full bg-gray-400 bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg">
                 {/* Black hole week container */}
                 <div className="black__hole__container">
-
+                    <MarsFacts />
                 </div>
             </div>
         </div>
