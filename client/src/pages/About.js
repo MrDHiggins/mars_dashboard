ad00fb3c1ff50faf8ab0e3a9b418132e7ce6f3de
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-15 03:06:17 +0200

    Cleaned up the About page and removed unwanted comments

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,21 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-const Immutable = require("immutable"); // import immutable module
 
 function About({ roverData, selectedRover }) {
-  // const { name, landing_date, launch_date, status } = roverData?.latest_photos[0]?.rover;
   const rover = roverData?.latest_photos?.[0]?.rover;
   const name = rover?.name;
   const landing_date = rover?.landing_date;
   const launch_date = rover?.launch_date;
   const status = rover?.status;
-  // data = Immutable.Map({...data}).toJS();
-  // data = JSON.stringify(data)
-  // console.log(data)
-  // setRover(data)
-  // let latestData = newData.merge(newData).toJS();
-  // latestData = latestData.toJS();
+
   const [photos, setPhotos] = useState([]);
 
   const roverName = selectedRover;
@@ -28,84 +21,57 @@ function About({ roverData, selectedRover }) {
       .catch((error) => console.log(error));
   }, [roverName]);
 
-  // if (data) {
-  // imgSrcArray = data.latest_photos.map((item) => ({
-  //   name: item.name,
-  //   landing_date: item.landing_date,
-  //   launch_date: item.launch_date,
-  //   status: item.status,
-  //   img_src: item.img_src
-  // }));
-  //console.log(data);
-  // }
-
-  // const imgSrcArray = data.latest_photos.map((item) => item.img_src);
-  // console.log(imgSrcArray);
-
   return (
-    <div className="flex">
-      <div className="about__container w-1/3">
-        <div className="rover__info__container h-full w-full bg-blue-500">
-          <div className="rover__name flex">
-            <h1 className="font-bold mr-1">Rover Name:</h1>
-            <p>{name}</p>
-          </div>
+    <div className="about__rover__container flex bg-blue-300 min-h-screen min-h-screen">
 
-          <div className="rover__landing__date flex">
-            <h1 className="font-bold mr-1">Landing Date: </h1>
-            <p>{landing_date}</p>
-          </div>
+      {/* start rover__info */}
+      <div className="rover__info w-1/3 p-3">
 
-          <div className="rover__launch__date flex">
-            <h1 className="font-bold mr-1">Launch Date: </h1>
-            <p>{launch_date}</p>
-          </div>
+        <div className="rover__name">
+          <h1 className="font-bold mr-1">Rover Name:</h1>
+          <p>{name}</p>
+        </div>
 
-          <div className="rover__status flex">
-            <h1 className="font-bold mr-1">Status: </h1>
-            <p>{status}</p>
-          </div>
+        <div className="rover__landing__date ">
+          <h1 className="font-bold mr-1">Landing Date: </h1>
+          <p>{landing_date}</p>
+        </div>
+
+        <div className="rover__launch__date ">
+          <h1 className="font-bold mr-1">Launch Date: </h1>
+          <p>{launch_date}</p>
+        </div>
+
+        <div className="rover__status ">
+          <h1 className="font-bold mr-1">Status: </h1>
+          <p>{status}</p>
         </div>
       </div>
-      <div className="carousel__container w-2/3 h-screen">
-        <div className="bg-green-500 h-full">
-          <Carousel axis={"horizontal"} dynamicHeight={false}>
+      {/* End rover__info */}
+
+      {/* start rover__carousel__container */}
+      <div className="rover__carousel__container w-2/3 bg-green-600 p-2">
+
+        <div className="rover__carousel">
+          <Carousel>
             {photos.map((photo) => (
-              <div key={photo} className="h-full">
+              <div className="rover__carousel__img h-full" key={photo}>
                 <img
-                  className="h-full w-full"
+                  className="object-fill h-[100%]"
                   src={photo}
                   alt={`Mars Rover img_src ID: ${photo}`}
                 />
-              </div>
+                </div>
             ))}
+            
           </Carousel>
         </div>
+
       </div>
+      {/* End rover__carousel__container */}
     </div>
+    // End about__rover__container
   );
 }
 
 export default About;
-{
-  /* {img_src && (
-              <Carousel>
-                {Array.isArray(latest_photos) ? (
-                  latest_photos.map((d) => (
-                    <div key={d.id}>
-                      <img
-                        src={d.img_src}
-                        alt={`Mars Rover img_src ID: ${d.id}`}
-                      />
-                      <p className="legend">{d.earth_date}</p>
-                    </div>
-                  ))
-                ) : (
-                  <div key={data.id}>
-                    <img src={data.img_src} alt={`Mars Rover img_src ID: ${data.id}`} />
-                    <p className="legend">{data.earth_date}</p>
-                  </div>
-                )}
-              </Carousel>
-            )} */
-}
