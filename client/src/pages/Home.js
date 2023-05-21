e498002db7794b7f67d73d60931a3030644273e9
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-22 00:34:34 +0200

    Refactored DOM structure and styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,16 +3,15 @@ import MarsFacts from '../components/home/MarsFacts';
 
 function Home() {
   return (
-    <div className='flex min-w-screen h-screen'>
-        <div className="home__container h-full w-full bg-nasa-ofTheDay p-2">
-            <div className="bg-red h-full w-full bg-gray-400 bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg">
-                {/* Black hole week container */}
-                <div className="black__hole__container">
-                    <MarsFacts />
-                </div>
-            </div>
+    <div className='flex justify-center items-center w-full h-screen bg-nasa-ofTheDay bg-cover'>
+      <div className="w-1/2 h-1/2 md:w-1/3 md:h-2/3 bg-gray-400 bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg">
+        {/* Black hole week container */}
+        <div className="h-1/4 flex justify-center items-center">
+          <MarsFacts />
         </div>
+      </div>
     </div>
   )
 }
 export default Home
+// bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg
