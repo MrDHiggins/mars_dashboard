e6f8ce3a8b2a2e72b91587528637cff8bd96e33a
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-22 03:51:01 +0200

    Reworked styling

--- client/src/components/home/GetRoverButtons.js
+++ client/src/components/home/GetRoverButtons.js
@@ -3,8 +3,10 @@ import RoverSelection from '../rover/RoverSelection';
 
 function GetRoverButtons({ setSelectedRover}) {
   return (
-    <div className='flex text-center w-full h-full justify-center'>
-      <div className='flex w-full items-center justify-center'>
+    <div className='text-center w-full h-full justify-center'>
+      <p className='justify-center mt-0 font-bold text-white'>Select A Rover:</p>
+      <div className='flex w-full h-2/3 items-center justify-center'>
+        
         <RoverSelection 
           btnStacked={true} 
           setSelectedRover={setSelectedRover} 
