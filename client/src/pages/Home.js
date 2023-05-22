70bae76af237e5564487be3b331f32c6e7d6cc68
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-22 03:41:24 +0200

    Renamed component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import MarsFacts from '../components/home/MarsFacts';
+import GetRoverButtons from '../components/home/GetRoverButtons';
 
-function Home() {
+
+function Home( {setSelectedRover }) {
   return (
-    <div className='flex justify-center items-center w-full h-screen bg-nasa-ofTheDay bg-cover'>
-      <div className="w-1/2 h-1/2 md:w-1/3 md:h-2/3 bg-gray-400 bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg">
-        {/* Black hole week container */}
-        <div className="h-1/4 flex justify-center items-center">
-          <MarsFacts />
-        </div>
+    <div className="flex justify-center items-center h-screen bg-nasa-ofTheDay bg-cover">
+      <div className="w-1/2 h-1/4 md:w-1/3 md:h-2/3 bg-gray-400 bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg">
+        <div className="h-full justify-center items-center">
+          <GetRoverButtons setSelectedRover={setSelectedRover} />
+          </div>
       </div>
     </div>
-  )
+  );
 }
-export default Home
-// bg-opacity-40 shadow-lg backdrop-filter backdrop-blur-sm border border-opacity-20 border-white rounded-lg
+
+export default Home;
