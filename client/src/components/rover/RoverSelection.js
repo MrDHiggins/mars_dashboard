4405ba4df8f41241789e96d699b21f416ee6e584
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-05-22 03:42:32 +0200

    Added dynamic styling

--- client/src/components/rover/RoverSelection.js
+++ client/src/components/rover/RoverSelection.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Immutable from 'immutable';
 import { useNavigate } from 'react-router-dom';
 
-function RoverSelection({ setSelectedRover }) {
+function RoverSelection({ setSelectedRover, btnStacked = false }) {
   const store = Immutable.Map({
     user: Immutable.Map({ name: 'Martian' }),
     apod: '',
@@ -13,6 +13,7 @@ function RoverSelection({ setSelectedRover }) {
   const [rovers, setRover] = useState(store.get('rovers'));
   const [selectedRover, setSelected] = useState(store.get('selectedRover'));
   const navigate = useNavigate();
+  const stackRoverButtons = btnStacked ? 'flex flex-col' : 'flex flex-row';
 
   const handleRoverSelection = (event, rovers) => {
     event.preventDefault();
@@ -25,10 +26,10 @@ function RoverSelection({ setSelectedRover }) {
   };
 
   const roverButtons = rovers.map((rover) => {
-    const selectedRoverBtn = selectedRover === rover ? 'bg-slate-400 text-w font-bold text-gray-700 border-t-2 border-gray-100' : 'bg-slate-500 text-gray-100';
+    const selectedRoverBtn = selectedRover === rover ? 'bg-slate-400 text-w font-bold text-gray-700 border-t-2 border-gray-100' : 'bg-slate-500 text-gray-100';  
     return (
       <button
-        className={`flex-shrink rounded hover:rounded border-gray-400 hover:border-t-2 hover:border-b-2 font hover:border-white  ${selectedRoverBtn} hover:font-bold `}
+        className={`rounded hover:rounded border-gray-400 hover:border-t-2 hover:border-b-2 font hover:border-white  ${selectedRoverBtn} hover:font-bold `}
         key={rover}
         value={rover}
         onClick={(event) => handleRoverSelection(event, rovers)}
@@ -41,7 +42,7 @@ function RoverSelection({ setSelectedRover }) {
   return (
     <div className="rover__selection__menu flex justify-center text-center">
       <div>
-        <div className='flex gap-1 lg:gap-5'>{roverButtons}</div>
+        <div className={`${stackRoverButtons} gap-1 lg:gap-5`}>{roverButtons}</div>
       </div>
     </div>
   );
